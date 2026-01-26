6b53141cee036dcf278dd546e0883281601e985d
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-01-26 20:26:01 +0100

    about.page

---
+++ generate-template.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+
+    Template Name: Generale Template
+
+
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id = "main" class="site-main">
+            <dive class="container">
+                <div class="general-template">
+                    <?php
+                        if(have_posts());
+                            while(have_posts(): the_post());
+
+                    ?>
+                    <article>
+                        <h1><?php the_title();?></h1>
+                        <?php the_contect(); ?>
+
+                    </article>
+
+                    <?php
+                        endwhile;
+                    else:
+                    ?>
+                    <p>Nothing yet to be displayed!</p>
+                    <?php endif; ?>
+
+                </div>
+            </dive>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ page-about.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+
+    Template Name: about page
+
+
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="content" class="site-content">
+    <main id="main" class ="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About Us</h1>
+                <p>welcome to our tech corner - where innovation meets explanation.</p>
+            </div>
+        </section>
+        <section class="about-intro">
+            <div class="container">
+                <h2>who we are</h2>
+                <p>we are a team of developers.</p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2>our mission </h2>
+                <p>our goal is simple...</p>
+            </div>
+        </section>
+        
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>what we cover</h2>
+                <ul>
+                    <li>programming tutorials and tips</li>
+                    <li>software and app reviews</li>
+                    <li>web deve;opment</li>
+                    <li>trends in tech and industry news.</li>
+                </ul>
+            </div>
+        </section>
+        <section class="about-contact">
+            <div class="container">
+                <h2>want to connect</h2>
+                <p>we love hearing from our readers!...</p>
+            </div>
+        </section>
+    </main>
+</div>
+
+<?php get_footer(); ?>
